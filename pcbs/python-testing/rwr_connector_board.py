ffe38c9b480e1000b98a398d6c87662959cb413a
R G Edgar <freesurfer.rge@gmail.com>
2024-10-19 10:58:54 -0400

    Trying to get things working

--- pcbs/python-testing/rwr_connector_board.py
+++ pcbs/python-testing/rwr_connector_board.py
@@ -1,8 +1,9 @@
-rom typing import Dict, List, Union
+from typing import Dict, List, Union
 
 import bitarray.util
 
 from tester_board import TesterBoard
+
 
 class RWRConnectorBoard:
     def __init__(self):
@@ -14,6 +15,8 @@ class RWRConnectorBoard:
 
         # Set all outputs low
         self._outputs = [False for _ in range(self._tb.n_pins)]
+        # ... except for RESET
+        self.Reset(True)
         self.send()
         self._tb.enable_outputs([True for _ in range(5)])
 
@@ -23,17 +26,17 @@ class RWRConnectorBoard:
     @property
     def Output_Pins(self) -> Dict[str, Union[int, List[int]]]:
         op = dict(
-            RegIn = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
-            Reset = 20,
-            Clock = 22,
-            OE = 40,
+            RegIn=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+            Reset=19,
+            Clock=21,
+            OE=39,
         )
         return op
 
     @property
     def Input_Pins(self) -> Dict[str, Union[int, List[int]]]:
         ip = dict(
-            RegOut = [23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38],
+            RegOut=[22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37],
         )
         return ip
 
@@ -43,15 +46,14 @@ class RWRConnectorBoard:
     def send(self):
         self._tb.send(self._outputs)
 
-
     def write_register(self, value: int):
         assert isinstance(value, int)
-        assert value>=0
-        assert value<65536
+        assert value >= 0
+        assert value < 65536
 
         converted = bitarray.util.int2ba(value, length=16, endian="little")
         for i, curr_bit in enumerate(converted):
-            self._outputs[self.OutputPins["RegIn"][i]] = curr_bit
+            self._outputs[self.Output_Pins["RegIn"][i]] = curr_bit
 
     def read_register(self) -> int:
         vals = []
@@ -63,8 +65,8 @@ class RWRConnectorBoard:
 
     def Reset(self, value: bool):
         self._outputs[self.Output_Pins["Reset"]] = value
-    
-    def OE(self, value:bool):
+
+    def OE(self, value: bool):
         self._outputs[self.Output_Pins["OE"]] = value
 
     def Clock(self, value: bool):
