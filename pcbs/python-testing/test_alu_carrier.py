85d2ce615c8e26d32da5c7e1813a54cad1d1dc08
R G Edgar <freesurfer.rge@gmail.com>
2025-02-09 18:59:07 -0500

    Get things working

--- pcbs/python-testing/test_alu_carrier.py
+++ pcbs/python-testing/test_alu_carrier.py
@@ -2,11 +2,26 @@ import pytest
 
 from pi_backplane import _Input, _Output
 
-bus_vals = [0, 1, 2989, 32767, 32768, 32769, 45534, 65535]
+bus_vals = [
+    0,
+    1,
+    127,
+    128,
+    129,
+    1533,
+    2989,
+    32767,
+    32768,
+    32769,
+    45534,
+    60235,
+    61233,
+    65534,
+    65535,
+]
 
-instructions = {
-    "compare" : 4
-    }
+instructions = {"compare": 4}
+
 
 @pytest.mark.parametrize("A", bus_vals)
 @pytest.mark.parametrize("B", bus_vals)
@@ -25,17 +40,18 @@ def test_comparator(A, B):
     output.send()
 
     active_cycles = [2, 3]
-    for c in range(5):
-        output.set_cycle(c)
+    for cyc in range(5):
+        output.set_cycle(cyc)
+        output.send()
         input.recv()
 
         c_val = input.read_bus("C")
-        if c in active_cycles:
+        if cyc in active_cycles:
             if A < B:
                 assert c_val == 1
             elif A > B:
                 assert c_val == 4
             else:
-                assert c== 2
+                assert c_val == 2
         else:
             assert c_val == 0
