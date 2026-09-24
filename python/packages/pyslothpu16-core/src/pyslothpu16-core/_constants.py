19ccd33a34409791e8651294e3cc66870bd222d0
Richard Edgar <freesurfer.rge@gmail.com>
2026-09-24 19:37:44 -0400

    Starting setup

---
+++ python/packages/pyslothpu16-core/src/pyslothpu16-core/_constants.py
@@ -0,0 +1,40 @@
+N_BITS = 16
+INSTR_BITS = 4
+REG_BITS = 4
+
+INSTRUCTIONS: dict[str, int] = {
+    "add": 0,
+    "sub": 1,
+    "compare": 4,
+    "nand": 5,
+    "xor": 6,
+    "barrel": 7,
+    "loadb": 8,
+    "loadw": 9,
+    "storeb": 10,
+    "storew": 11,
+    "loadpc": 12,
+    "branchzero": 13,
+    "halt": 14,
+    "set": 15,
+}
+
+INSTR_DECODE = [
+    "add",
+    "sub",
+    "",
+    "",
+    "compare",
+    "nand",
+    "xor",
+    "barrel",
+    "loadb",
+    "loadw",
+    "storeb",
+    "storew",
+    "loadpc",
+    "branchzero",
+    "halt",
+    "set",
+]
+assert len(INSTR_DECODE) == 2**INSTR_BITS
